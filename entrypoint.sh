17f8386a39bda4a4f67d1a4f8a00fea3852ab666
Brad Cowie <brad@wand.net.nz>
2020-11-17 14:59:37 +1300

    shellcheck

--- entrypoint.sh
+++ entrypoint.sh
@@ -14,20 +14,19 @@ export LANG=C.UTF-8
 function upload_rpm {
     distro=$2
     pkg_filename="$(basename "$1")"
-    rev_filename=`echo ${pkg_filename} | rev`
+    rev_filename=$(echo "${pkg_filename}" | rev)
 
-    pkg_name=`echo ${rev_filename} | cut -d '-' -f3- | rev`
-    pkg_version=`echo ${rev_filename} | cut -d '-' -f1-2 | rev | cut -d '.' -f1-3`
-    pkg_arch=`echo ${rev_filename} | cut -d '.' -f2 | rev`
-    pkg_rel=`echo ${rev_filename} | cut -d '.' -f3 | rev`
-    releasever="${pkg_rel:2}"
+    pkg_name=$(echo "${rev_filename}" | cut -d '-' -f3- | rev)
+    pkg_version=$(echo "${rev_filename}" | cut -d '-' -f1-2 | rev | cut -d '.' -f1-3)
+    pkg_arch=$(echo "${rev_filename}" | cut -d '.' -f2 | rev)
+    pkg_rel=$(echo "${rev_filename}" | cut -d '.' -f3 | rev)
+    release_ver="${pkg_rel:2}"
 
-    cloudsmith push rpm ${CLOUDSMITH_REPO}/${distro}/${releasever} ${1}
+    cloudsmith push rpm "${CLOUDSMITH_REPO}/${distro}/${release_ver}" "${1}"
 }
 
 function upload_deb {
-
-    cloudsmith push deb ${CLOUDSMITH_REPO}/${2}/${3} ${1}
+    cloudsmith push deb "${CLOUDSMITH_REPO}/${2}/${3}" "${1}"
 }
 
 function cloudsmith_upload {
@@ -36,11 +35,11 @@ function cloudsmith_upload {
     pkg_fullpath=$3
 
     if [[ ${linux_distro} =~ centos ]]; then
-	upload_rpm ${pkg_fullpath} "centos"
+	upload_rpm "${pkg_fullpath}" "centos"
     elif [[ ${linux_distro} =~ fedora ]]; then
-	upload_rpm ${pkg_fullpath} "fedora"
+	upload_rpm "${pkg_fullpath}" "fedora"
     else
-	upload_deb ${pkg_fullpath} ${linux_distro} ${linux_release}
+	upload_deb "${pkg_fullpath}" "${linux_distro}" "${linux_release}"
     fi
 }
 
@@ -54,5 +53,3 @@ do
 	cloudsmith_upload "${distro}" "${release}" "${pkg}"
     done <    <(find "${path}" -maxdepth 1 -type f -print0)
 done <   <(find "${PACKAGE_LOCATION}" -mindepth 1 -maxdepth 1 -type d -print0)
-
-
