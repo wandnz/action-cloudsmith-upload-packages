42b041e0bf707fd26c19dcba05ea5c67b2b74a2d
Brad Cowie <brad@wand.net.nz>
2021-01-15 12:17:50 +1300

    refactor

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,7 +7,7 @@ CLOUDSMITH_REPO="${2}"
 CLOUDSMITH_USERNAME="${3}"
 export CLOUDSMITH_API_KEY="${4}"
 
-cloudsmith_push_args=(--error-retry-max 30 --republish)
+cloudsmith_default_args=(--error-retry-max 30 --republish)
 
 # required to make python 3 work with cloudsmith script
 export LC_ALL=C.UTF-8
@@ -26,12 +26,12 @@ function upload_rpm {
     pkg_rel=$(echo "${rev_filename}" | cut -d '.' -f3 | rev)
     release_ver="${pkg_rel:2}"
 
-    sync_arg=""
+    extra_args=("${cloudsmith_default_args[@]}")
     if [ "${sync}" == "nosync" ]; then
-        sync_arg="--no-wait-for-sync"
+        extra_args+=(--no-wait-for-sync)
     fi
 
-    cloudsmith push rpm ${sync_arg} "${cloudsmith_push_args[@]}" "${CLOUDSMITH_REPO}/${distro}/${release_ver}" "${pkg_fullpath}"
+    cloudsmith push rpm "${extra_args[@]}" "${CLOUDSMITH_REPO}/${distro}/${release_ver}" "${pkg_fullpath}"
 }
 
 function upload_deb {
@@ -40,12 +40,12 @@ function upload_deb {
     release=$3
     pkg_fullpath=$4
 
-    sync_arg=""
+    extra_args=("${cloudsmith_default_args[@]}")
     if [ "${sync}" == "nosync" ]; then
-        sync_arg="--no-wait-for-sync"
+        extra_args+=(--no-wait-for-sync)
     fi
 
-    cloudsmith push deb "${sync_arg}" "${cloudsmith_push_args[@]}" "${CLOUDSMITH_REPO}/${distro}/${release}" "${pkg_fullpath}"
+    cloudsmith push deb "${extra_args[@]}" "${CLOUDSMITH_REPO}/${distro}/${release}" "${pkg_fullpath}"
 }
 
 function cloudsmith_upload {
@@ -84,6 +84,6 @@ while IFS= read -r -d '' path; do
         else
             cloudsmith_upload "nosync" "${distro}" "${release}" "${pkg}"
         fi
-        ((i++))
+        i=$((i+1))
     done
 done <   <(find "${PACKAGE_LOCATION}" -mindepth 1 -maxdepth 1 -type d -print0)
